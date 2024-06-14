6350c62d6e46cfff7e3a730797cff9db07eed332
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-06-14 17:02:15 +0530

    * CORS Fixes.

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const express = require('express')
-const mailRouter = require('./Routes/mailRoute')
+const express = require("express")
+const mailRouter = require("./Routes/mailRoute")
 const app = express()
-const cors = require("cors");
+const cors = require("cors")
 const port = 3000
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://www.vcwebstudio.com",
-    "https://vc-web-studio.vercel.app",
-  ];
+const allowedOrigins = ["http://localhost:5173", "https://www.vcwebstudio.com", "https://vc-web-studio.vercel.app"]
 
-  const corsOptions ={
-    origin:allowedOrigins, 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-    methods: ["GET","POST"]
- }
- 
- app.use(cors(corsOptions))
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+  methods: ["GET", "POST"],
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.options("*", cors())
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
+}
+app.use(allowCrossDomain)
+app.use(cors(corsOptions))
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
 })
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use("/Mail",mailRouter)
+app.use("/Mail", mailRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
