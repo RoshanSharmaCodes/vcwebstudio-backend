01f41b823ad56556a480a859afbee9956f0e9b0e
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-06-14 16:42:25 +0530

    * index.js cors function change.

--- index.js
+++ index.js
@@ -10,21 +10,13 @@ const allowedOrigins = [
     "https://vc-web-studio.vercel.app",
   ];
 
-app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-  
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg =
-            "The CORS policy for this site does not allow access from the specified origin.";
-          return callback(new Error(msg), false);
-        }
-  
-        return callback(null, true);
-      },
-    })
-  );
+  const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
