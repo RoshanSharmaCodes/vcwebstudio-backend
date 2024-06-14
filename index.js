2bcecc6a131969b3a4313215b39532a5c497facf
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-06-14 17:08:44 +0530

    * CORS Fixes.

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-const express = require("express")
-const mailRouter = require("./Routes/mailRoute")
-const app = express()
-const cors = require("cors")
-const port = 3000
+const express = require("express");
+const mailRouter = require("./Routes/mailRoute");
+const app = express();
+const cors = require("cors");
+const port = 3000;
 
-const allowedOrigins = ["http://localhost:5173", "https://www.vcwebstudio.com", "https://vc-web-studio.vercel.app"]
+const allowedOrigins = ["http://localhost:5173", "https://www.vcwebstudio.com", "https://vc-web-studio.vercel.app"];
 
 const corsOptions = {
   origin: allowedOrigins,
-  credentials: true, //access-control-allow-credentials:true
+  credentials: true,
   optionSuccessStatus: 200,
   methods: ["GET", "POST"],
-}
+};
 
-app.options("*", cors())
-var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
-  res.header("Access-Control-Allow-Headers", "Content-Type")
-  next()
-}
-app.use(allowCrossDomain)
-app.use(cors(corsOptions))
+app.use(cors(corsOptions)); // Applying CORS middleware here
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-app.use("/Mail", mailRouter)
+app.use("/Mail", mailRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
