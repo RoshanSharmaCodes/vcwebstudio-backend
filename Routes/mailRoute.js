9d5eefa5247c5dc27602e4f671fd1432cf94e93f
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-06-14 16:11:30 +0530

    backend initialized

---
+++ Routes/mailRoute.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mailRouter = express.Router();
+const nodemailer = require('nodemailer');
+
+mailRouter.get('/', (req, res) => {
+    console.log("Req")
+    res.send('Mail route is working!');
+});
+
+mailRouter.post("/sendmail", async (req, res) => {
+    const requestBody = req.body;
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtpout.secureserver.net',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'roshan.rks2812000@gmail.com',
+            pass: '8090247890',
+        },
+    });
+
+   if (!requestBody.message || requestBody.message == "") {
+        res.status(400).json({ ErrorCode: "E01", Message: "Missing Email Body" })
+        return;
+}
+
+    const mailOptions = {
+        from: 'VCWebStudio Team support@vcwebstudio.tech',
+        to: ["sushrut@vcmedialabs.com","roshan.rks2812000@gmail.com", requestBody.email],
+        text: requestBody.message,
+    };
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        if(info){
+            const mailSendToClient = {
+                from: 'VCWebStudio Team roshan.rks2812000@gmail.com',
+                to: requestBody.email,
+                subject: "Thank You for Your Inquiry",
+                html: `<p>Dear ${requestBody.name},</p><p>Thank you for reaching out to us. We have received your query and appreciate your interest in our services.
+                <br/>
+                Our team is currently reviewing your request and will get back to you shortly with more information.
+                <br/>
+                In the meantime, if you have any urgent questions or concerns, feel free to reach out to us at sales@vcwebstudio.tech.
+                <br/>
+                <br/>
+                Thank you for considering us, and we look forward to the opportunity to assist you further.</p><br/><p>Best regards,<br/>Team VCWebStudio.</p>`,
+            };
+            const info = await transporter.sendMail(mailSendToClient);
+        }
+
+        res.status(200).json({ErrorCode:"E00",Message:'Email sent successfully!'});
+    } catch (error) {
+        console.error('Error sending email: ', error.message);
+
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+
+module.exports = mailRouter;
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mailRouter = require('./Routes/mailRoute')
+const app = express()
+const cors = require("cors");
+const port = 3000
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://www.vcwebstudio.com",
+  ];
+
+app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+  
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg =
+            "The CORS policy for this site does not allow access from the specified origin.";
+          return callback(new Error(msg), false);
+        }
+  
+        return callback(null, true);
+      },
+    })
+  );
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/Mail",mailRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
